5640218c249b2e34b2871174cf5a8f520dac6dd2
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-04-12 21:07:00 +0300

    change components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Info from './components/info/info.js';
+import Info from './components/info/info.jsx';
 
 ReactDOM.render(
   <Info />,
